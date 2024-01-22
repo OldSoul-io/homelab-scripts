4a338cbf850da277405e12a7d79365ea183e77a6
OldSoul.io <joshua@oldsoul.io>
2024-01-22 17:29:25 -0500

    Updated source paths

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,9 +4,9 @@
 sudo bash -c '
 
 # UPDATE SCRIPT SOURCE
-source /utils/error_handling.sh
-source /utils/checks.sh
-source /utils/secrets.sh
+source ./utils/error_handling.sh
+source ./utils/checks.sh
+source ./utils/secrets.sh
 
 # SET UP ERROR HANDLING
 setUpErrorHandling
@@ -19,26 +19,26 @@ apt update && apt full-upgrade -y
 
 # ### 2. Install sudo, zsh, fail2ban, and ufw ###
 echo ">>> Setting up security measures."
-source /modules/install_sudo.sh
-source /modules/install_zsh.sh
-source /modules/install_fail2ban.sh
-source /modules/install_ufw.sh
+source ./modules/install_sudo.sh
+source ./modules/install_zsh.sh
+source ./modules/install_fail2ban.sh
+source ./modules/install_ufw.sh
 
 
 # ### 3. Install 1Password CLI tool ###
-source /modules/install_1password.sh
+source ./modules/install_1password.sh
 
 
 # ### 4. Check if users already exists ###
-source /modules/adduser_oldsoul+io.sh
+source ./modules/adduser_oldsoul+io.sh
 
 
 # ### 5. Install Tailscale ###
-source /modules/install_tailscale.sh
+source ./modules/install_tailscale.sh
 
 
 # ### 6. Install Yai ###
-source /modules/install_yai.sh
+source ./modules/install_yai.sh
 '
 
 echo "Setup script completed successfully."
