eaf0e2f01141913a20764a3222c598a313d062ec
OldSoul.io <joshua@oldsoul.io>
2024-01-26 12:21:06 -0500

    Updated sourcing of secrets to avoid install error

--- dev_setup.sh
+++ dev_setup.sh
@@ -6,7 +6,6 @@ sudo bash -c '
 # UPDATE SCRIPT SOURCE
 source ./utils/error_handling.sh
 source ./utils/checks.sh
-source ./utils/secrets.sh
 
 # SET UP ERROR HANDLING
 setUpErrorHandling
@@ -15,19 +14,18 @@ setUpErrorHandling
 # ### 1. Update and upgrade packages ###
 echo ">>> Updating and upgrading packages..."
 apt update && apt full-upgrade -y
-
-
-# ### 2. Install sudo, zsh, fail2ban, and ufw ###
-echo ">>> Setting up security measures."
 source ./modules/install_sudo.sh
+
+# ### 2. Install 1Password CLI tool ###
+source ./modules/install_1password.sh
+source ./utils/secrets.sh
+
+
+# ### 3. Install sudo, zsh, fail2ban, and ufw ###
+echo ">>> Setting up security measures."
 source ./modules/install_zsh.sh
 source ./modules/install_fail2ban.sh
 source ./modules/install_ufw.sh
-
-
-# ### 3. Install 1Password CLI tool ###
-source ./modules/install_1password.sh
-
 
 # ### 4. Check if users already exists ###
 source ./modules/adduser_oldsoul+io.sh
